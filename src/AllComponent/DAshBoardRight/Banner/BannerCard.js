c10d31a61cde746699ba00ca2331f965de2ef1d9
Rajdeepak <rajdeepakvishwakarma24@gmail.com>
2024-02-15 00:23:26 +0530

    small changes

--- src/AllComponent/DAshBoardRight/Banner/BannerCard.js
+++ src/AllComponent/DAshBoardRight/Banner/BannerCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import DeleteIcon from "@mui/icons-material/Delete";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
@@ -6,46 +6,86 @@ import Button from "@mui/material/Button";
 import img from "../../../Media/Images/banner2.jpg";
 import Typography from "@mui/material/Typography";
 import ModeIcon from "@mui/icons-material/Mode";
-
+import { styled } from "@mui/material/styles";
+const VisuallyHiddenInput = styled("input")({
+  clip: "rect(0 0 0 0)",
+  clipPath: "inset(50%)",
+  height: 1,
+  overflow: "hidden",
+  position: "absolute",
+  bottom: 0,
+  left: 0,
+  whiteSpace: "nowrap",
+  width: 1,
+});
 const BannerCard = (props) => {
+  const [storedFilePath, setStoredFilePath] = useState([]);
+
+  const handleImageUpload = (value, id) => {
+    let storedPath = [...storedFilePath];
+    let data = { id: id, value: value };
+    storedPath.push(data);
+    setStoredFilePath(storedPath);
+    console.log("value and id", storedPath);
+    console.log("storedFilePath", storedFilePath);
+  };
   return (
     <>
+      {/* {console.log("storedFileP====ath", storedFilePath)} */}
       {props.Data.map((value, index) => (
-       <div className="BannerMainBox">
-       <div className="InsideBannerBox">
-          <main className="InsideMainBox" key={index}>
-            <CardActions className="BannerHead">
-              <Typography>{value.Head}</Typography>
-              <Button className="Deletebtn">
-                <DeleteIcon className="Deleteicon" />
-                Delete
-              </Button>
-            </CardActions>
-
-            <div class="container">
-              <img
-                src={value.img}
-                alt="Avatar"
-                className="BannerImage image"
-                // style={{width:"100%"}}
-              />
-              <div class="middle">
-                <Button class="text">
-                  {" "}
-                  <ModeIcon className="PencilIcon" />
-                  change
+        <div className="BannerMainBox">
+          <div className="InsideBannerBox">
+            <main className="InsideMainBox" key={index}>
+              <CardActions className="BannerHead">
+                <Typography>{value.Head}</Typography>
+                <Button className="Deletebtn">
+                  <DeleteIcon className="Deleteicon" />
+                  Delete
                 </Button>
+              </CardActions>
+
+              <div class="container">
+                <img
+                  src={value.img}
+                  alt="Avatar"
+                  className="BannerImage image"
+                  // style={{width:"100%"}}
+                />
+                <div class="middle">
+                  <Button class="text" component="label">
+                    {" "}
+                    <ModeIcon className="PencilIcon" />
+                    change
+                    <VisuallyHiddenInput
+                      type="file"
+                      accept="image/*"
+                      onChange={(event) =>
+                        handleImageUpload(event.target.value, value.id)
+                      }
+                    />
+                  </Button>
+                </div>
               </div>
-            </div>
-            <CardContent>
-              <p className="fontSize1">{value.title}</p>
-            </CardContent>
-            <div className="BannerHead BorderBottom">
-              <p>{value.boxtitle}</p>
-              <Button className="changeBtn">Change</Button>
-            </div>
-          </main>
-        </div>
+              <div>kjhbn</div>
+              {/* {storedFilePath.map((item) => {
+                return item.value;
+              })} */}
+              {console.log("storedFilePathksdfv",storedFilePath)}
+              {storedFilePath?.map((row) => {
+                console.log("71", row.id, row.value);
+                console.log("72", value.id);
+                return row.id === value.id ? row.value : null;
+              })}
+
+              <CardContent>
+                <p className="fontSize1">{value.title}</p>
+              </CardContent>
+              <div className="BannerHead BorderBottom">
+                <p>{value.boxtitle}</p>
+                <Button className="changeBtn">Change</Button>
+              </div>
+            </main>
+          </div>
         </div>
       ))}
     </>
--- src/AllComponent/DAshBoardRight/Testimonial/Testimonial.js
+++ src/AllComponent/DAshBoardRight/Testimonial/Testimonial.js
@@ -1,7 +1,7 @@
-import React, { Fragment } from "react";
+import React, {Fragment} from "react";
 import CourseHeader from "../../Courses/CoursesHeader";
-import FilterAltIcon from "@mui/icons-material/FilterAlt";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
+import FilterAltIcon from '@mui/icons-material/FilterAlt';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 import Paper from "@mui/material/Paper";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -13,17 +13,18 @@ import TableRow from "@mui/material/TableRow";
 import SearchBar from "../../../Util/SearchBar";
 import Button from "@mui/material/Button";
 import Popover from "@mui/material/Popover";
-import DeleteIcon from "@mui/icons-material/Delete";
-import Typography from "@mui/material/Typography";
+import DeleteIcon from '@mui/icons-material/Delete';
+import Typography from '@mui/material/Typography';
 import SideBar from "../../AdminDashboardMain/SideBar";
 
 const Testimonial = () => {
+
   const columns = [
     {
       id: "Comments",
       minWidth: 100,
       label: "Comments",
-      align: "left",
+      align:  "left",
     },
     {
       id: "Author",
@@ -39,11 +40,11 @@ const Testimonial = () => {
 
   const createData = (Comments, Author, Actions) => {
     return { Comments, Author, Actions };
-  };
+  }
 
   const rows = [
     createData(
-      "Can't express the amount of respect to all the members in this platform who are working so hard for us. We are so blessed to have this lovely platform in our generation!",
+     "Can't express the amount of respect to all the members in this platform who are working so hard for us. We are so blessed to have this lovely platform in our generation!" ,
       "Sheikh Shoeb",
       <MoreVertIcon
         onClick={(event) =>
@@ -85,7 +86,6 @@ const Testimonial = () => {
 
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
-  const [Filter, setFilter] = React.useState(null);
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
@@ -110,141 +110,40 @@ const Testimonial = () => {
 
   const open = Boolean(anchorEl);
   const id = open ? "simple-popover" : undefined;
-
-  const handleFilterClick = (event) => {
-    setFilter(event.currentTarget);
-  };
-
-  const handleFilterClose = () => {
-    setFilter(null);
-  };
-
-  const Filteropen = Boolean(Filter);
-  const Filterid = Filteropen ? 'simple-popover' : undefined;
   return (
-    <div className="grid-container">
+    <div className='grid-container'>
       <SideBar />
-      <div className="main-container">
-        <div className="m20">
-          <CourseHeader
-            Heading={"Testimonial"}
-            subHeading={"Only published testimonial are shown here"}
-          />
-          <div className="testPortalSearchBarSection">
-            <div className="searchnfilter">
-              <SearchBar mt="2%" placeholder="Search by name" />
-                <Button className="filterButton" onClick={handleFilterClick}>
-                  <FilterAltIcon /> Filter
-                </Button>
-                <Popover
-                  id={Filterid}
-                  open={Filteropen}
-                  anchorEl={Filter}
-                  onClose={handleFilterClose}
-                  anchorOrigin={{
-                    vertical: "bottom",
-                    horizontal: "left",
-                  }}
-                  sx={{mt: 1}}
-                >
-                  <Typography sx={{padding: "5px 30px"}}>
-                    Name 
-                  </Typography>
-                  <Typography sx={{padding: "5px 30px" }}>
-                    Phone
-                  </Typography>
-                  <Typography sx={{padding: "5px 30px" }}>
-                    Email
-                  </Typography>
-                </Popover>
-            </div>
-            <Button className="addTestimonialButton">
-              {" "}
-              + Add Testimonial{" "}
+    <div className="main-container">
+      <div className="m20">
+        <CourseHeader Heading={"Testimonial"} subHeading={"Only published testimonial are shown here"}/>
+        <div className="testPortalSearchBarSection">
+          <div className="searchnfilter">
+            
+            <SearchBar mt ="2%" placeholder="Search by name" />
+            
+            <Button className="filterButton">
+              <FilterAltIcon /> Filter
             </Button>
           </div>
+          <Button className="addTestimonialButton"> + Add Testimonial </Button>
+        </div>
 
-          <Paper
-            sx={{ width: "100%", overflow: "hidden" }}
-            className="completeTable"
-          >
-            <TableContainer sx={{ maxHeight: 540 }}>
-              <Table stickyHeader aria-label="sticky table">
-                <TableHead>
-                  <TableRow>
-                    {columns.map((column) => (
-                      <TableCell
-                        key={column.id}
-                        align={column.align}
-                        style={{ width: "56%", fontWeight: 600 }}
-                        className="headingOfTable"
-                      >
-                        {column.label}
-                      </TableCell>
-                    ))}
-                  </TableRow>
-                </TableHead>
-                <TableBody className="parentTable">
-                  {rows
-                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                    .map((row) => {
-                      return (
-                        <TableRow
-                          hover
-                          className="TableHover"
-                          role="checkbox"
-                          tabIndex={-1}
-                          key={row.code}
-                        >
-                          {columns.map((column) => {
-                            const value = row[column.id];
-                            console.log(
-                              row,
-                              Object.keys(row),
-                              column.id,
-                              value
-                            );
-                            return (
-                              <Fragment>
-                                {column.id === "Commments" ? (
-                                  <TableCell
-                                    key={column.id}
-                                    align={column.align}
-                                    className="lastTableCellOfTestimonial"
-                                  >
-                                    <Typography className="lastValue">
-                                      {value}
-                                    </Typography>
-                                    <Typography className="phNumber">
-                                      FREE TEST
-                                    </Typography>
-                                  </TableCell>
-                                ) : (
-                                  <TableCell
-                                    key={column.id}
-                                    align={column.align}
-                                    sx={{ minWidth: 100 }}
-                                  >
-                                    {value}
-                                  </TableCell>
-                                )}
-                              </Fragment>
-                            );
-                          })}
-                        </TableRow>
-                      );
-                    })}
-                  <Popover
-                    id={openId}
-                    open={open}
-                    anchorEl={anchorEl}
-                    onClose={handleClose}
-                    anchorOrigin={{
-                      vertical: "bottom",
-                      horizontal: "right",
-                    }}
+
+<Paper
+        sx={{ width: "100%", overflow: "hidden" }}
+        className="completeTable"
+      >
+        <TableContainer sx={{ maxHeight: 540 }}>
+          <Table stickyHeader aria-label="sticky table">
+            <TableHead>
+              <TableRow>
+                {columns.map((column) => (
+                  <TableCell
+                    key={column.id}
+                    align={column.align}
+                    style={{ width: "56%", fontWeight: 600 }}
+                    className="headingOfTable"
                   >
-<<<<<<< HEAD
                     {column.label}
                   </TableCell>
                 ))}
@@ -287,7 +186,6 @@ const Testimonial = () => {
                   );
                 })}
               <Popover
-               sx={{m: -7, mt: 0.7}}
                 id={openId}
                 open={open}
                 anchorEl={anchorEl}
@@ -298,30 +196,17 @@ const Testimonial = () => {
                 }}
               >
 
-                {/* <Typography sx={{ p: 0.7 }} className='redDelete'><DeleteIcon/> Delete</Typography> */}
-                <Typography className="redDeleteofTestPortal">
-                    <DeleteIcon className="deleteIcon"/> <span>Delete</span>
-                  </Typography>
+                <Typography sx={{ p: 1 }} className='redDelete'><DeleteIcon/> Delete</Typography>
               </Popover>
             </TableBody>
           </Table>
         </TableContainer>
       </Paper>
 
-=======
-                    <Typography sx={{ p: 1 }} className="redDelete">
-                      <DeleteIcon /> Delete
-                    </Typography>
-                  </Popover>
-                </TableBody>
-              </Table>
-            </TableContainer>
-          </Paper>
-        </div>
->>>>>>> 03e02ee9bc3051f83adf16a0366ddd1f1d5f4a96
       </div>
     </div>
+    </div>
   );
-};
+}
 
 export default Testimonial;
