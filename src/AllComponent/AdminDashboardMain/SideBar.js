e869354cfe9210827a2be17d88ac00c3d787b30c
Shrishti2698 <shrishti260498@gmail.com>
2024-03-14 23:41:49 +0530

    sd

--- src/AllComponent/AdminDashboardMain/SideBar.js
+++ src/AllComponent/AdminDashboardMain/SideBar.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Logo from "../../Media/Logo.png";
 import MenuBookIcon from "@mui/icons-material/MenuBook";
@@ -20,7 +20,6 @@ function SideBar({ openSidebarToggle, OpenSidebar }) {
   const [anchorEl, setAnchorEl] = useState(null);
   const [anchorE2, setAnchorE2] = useState(null);
   const [anchorE3, setAnchorE3] = useState(null);
-
   const opens = Boolean(anchorE2);
   const ids = opens ? "simple-popover" : undefined;
   let currentlyHovering = false;
@@ -32,13 +31,12 @@ function SideBar({ openSidebarToggle, OpenSidebar }) {
   const Courseopen = Boolean(anchorE3);
   const idss = Courseopen ? "simple-popover" : undefined;
   const [hideUnHide, setHideUnHide] = useState(false);
-  const [hideSubConfig , setHideSubConfig] = useState(false);
+  const [hideSubConfig, setHideSubConfig] = useState(false);
   const [selectedConfigValue, setSelectedConfigValue] = useState("");
 
   const handlePopoverOpen = (event) => {
     setAnchorE2(event.currentTarget);
   };
-
   const handlePopoverClose = () => {
     setAnchorE2(null);
   };
@@ -54,36 +52,12 @@ function SideBar({ openSidebarToggle, OpenSidebar }) {
   const handleCloseCourse = () => {
     setAnchorE3(null);
   };
-
   const handleClick2 = (event) => {
     setAnchorE2(event.currentTarget);
   };
   const handleClose2 = () => {
     setAnchorE2(null);
   };
-
-  function handleClickNew(event) {
-    if (anchorE2 !== event.currentTarget) {
-      setAnchorE2(event.currentTarget);
-    }
-  }
-
-  function handleHover() {
-    currentlyHovering = true;
-  }
-
-  function handleCloseNew() {
-    setAnchorE2(null);
-  }
-
-  function handleCloseHover() {
-    currentlyHovering = false;
-    setTimeout(() => {
-      if (!currentlyHovering) {
-        handleCloseNew();
-      }
-    }, 50);
-  }
 
   // collapse
   const navigate = useNavigate();
@@ -93,11 +67,9 @@ function SideBar({ openSidebarToggle, OpenSidebar }) {
   const handleClickPeople = () => {
     setOpenPeople(!openPeople);
   };
-
   const handleCatConfig = (value) => {
     setHideCatConfig(true);
-    setSelectedConfigValue(value)
-
+    setSelectedConfigValue(value);
   };
   const handleCloseCat = () => {
     setHideCatConfig(false);
@@ -111,12 +83,45 @@ function SideBar({ openSidebarToggle, OpenSidebar }) {
   };
 
   const [highlight, setHighlight] = useState(false);
-  const handleHighlight = () =>{
-    setHighlight(true);
+  useEffect(() => {
+    setHighlight(localStorage.getItem("activeMenu"));
+  }, [localStorage.getItem("activeMenu")]);
+
+  const handleHighlight = (type) => {
+    setHighlight(type);
+    localStorage.setItem("activeMenu", type);
+    console.log("dashboard", type);
+    console.log("testPortal", type);
+    console.log("chats", type);
+    console.log("analytics", type);
+    console.log("testimonials", type);
+    console.log("myCourses", type);
+    console.log("manageCoupons", type);
+    console.log("catagory", type);
+    console.log("upcomingCourses", type);
   };
 
-  const handleHideSubCat=()=>{
+  const handleHideSubCat = () => {
     setHideSubConfig(!hideSubConfig);
+  };
+  function handleClickNew(event) {
+    if (anchorE2 !== event.currentTarget) {
+      setAnchorE2(event.currentTarget);
+    }
+  }
+  function handleHover() {
+    currentlyHovering = true;
+  }
+  function handleCloseNew() {
+    setAnchorE2(null);
+  }
+  function handleCloseHover() {
+    currentlyHovering = false;
+    setTimeout(() => {
+      if (!currentlyHovering) {
+        handleCloseNew();
+      }
+    }, 50);
   }
 
   return (
@@ -136,10 +141,12 @@ function SideBar({ openSidebarToggle, OpenSidebar }) {
 
         <div className="sidebar-list sidebar-list-item BottomLine">
           <Link to="/DashBoard">
-            <Typography 
-            id="hoverrr"
-            className={highlight ? "hoverrr2" : ""}
-             sx={{ mt: 1 }} onClick={handleHighlight}>
+            <Typography
+              id="hoverrr"
+              className={highlight === "dashboard" ? "hoverrr2" : ""}
+              sx={{ mt: 1 }}
+              onClick={() => handleHighlight("dashboard")}
+            >
               <DashboardIcon className="icon" />
               DashBoard
             </Typography>
@@ -161,11 +168,15 @@ function SideBar({ openSidebarToggle, OpenSidebar }) {
                   <li className="myCourses">
                     <Link to="/YourCourses" className="textDecoration">
                       <Typography
-                        className="textDecoration"
-                        sx={{
-                          textDecoration: "none",
-                          color: "greys",
-                        }}
+                        id="hoverrr"
+                        className={highlight === "myCourses" ? "hoverrr2" : ""}
+                        sx={{ mt: 1 }}
+                        onClick={() => handleHighlight("myCourses")}
+                        // className="textDecoration"
+                        // sx={{
+                        //   textDecoration: "none",
+                        //   color: "greys",
+                        // }}
                       >
                         My Courses
                       </Typography>
@@ -174,11 +185,12 @@ function SideBar({ openSidebarToggle, OpenSidebar }) {
                   <li className="listDesign">
                     <Link to="/CreateCoupon" className="textDecoration">
                       <Typography
-                        className="textDecoration"
-                        sx={{
-                          textDecoration: "none",
-                          color: "greys",
-                        }}
+                        id="hoverrr"
+                        className={
+                          highlight === "manageCoupons" ? "hoverrr2" : ""
+                        }
+                        sx={{ mt: 1 }}
+                        onClick={() => handleHighlight("manageCoupons")}
                       >
                         Manage Coupons
                       </Typography>
@@ -187,11 +199,10 @@ function SideBar({ openSidebarToggle, OpenSidebar }) {
                   <li className="listDesign">
                     <Link to="/User" className="textDecoration">
                       <Typography
-                        className="textDecoration"
-                        sx={{
-                          textDecoration: "none",
-                          color: "greys",
-                        }}
+                        id="hoverrr"
+                        className={highlight === "catagory" ? "hoverrr2" : ""}
+                        sx={{ mt: 1 }}
+                        onClick={() => handleHighlight("catagory")}
                       >
                         Category / Sub Catoggry
                       </Typography>
@@ -200,47 +211,58 @@ function SideBar({ openSidebarToggle, OpenSidebar }) {
                   <li className="listDesign">
                     <Link to="/YourCourses" className="textDecoration">
                       <Typography
-                        className="textDecoration"
-                        sx={{
-                          textDecoration: "none",
-                          color: "greys",
-                        }}
+                        // className="textDecoration"
+                        // sx={{
+                        //   textDecoration: "none",
+                        //   color: "greys",
+                        // }}
+                        id="hoverrr"
+                        className={
+                          highlight === "upcomingCourses" ? "hoverrr2" : ""
+                        }
+                        sx={{ mt: 1 }}
+                        onClick={() => handleHighlight("upcomingCourses")}
                       >
                         Upcoming Course / Blogs
                       </Typography>
                     </Link>
                   </li>
                 </ul>
-            
               </List>
             </Collapse>
           </Link>
-
+          {console.log("highlight", highlight)}
           <Link to="/TestPortal">
-            <Typography 
-            id="hoverrr"
-            className={highlight ? "hoverrr2" : ""}
-             sx={{ mt: -2 }} onClick={handleHighlight}>
+            <Typography
+              id="hoverrr"
+              className={highlight === "testPortal" ? "hoverrr2" : ""}
+              sx={{ mt: -2 }}
+              onClick={() => handleHighlight("testPortal")}
+            >
               <AssignmentIcon className="icon" />
               Test Portal
             </Typography>
           </Link>
 
           <Link to="/Chat">
-            <Typography 
-            id="hoverrr"
-            className={highlight ? "hoverrr2" : ""}
-             sx={{ mt: -2 }} onClick={handleHighlight}>
+            <Typography
+              id="hoverrr"
+              className={highlight === "chats" ? "hoverrr2" : ""}
+              sx={{ mt: -2 }}
+              onClick={() => handleHighlight("chats")}
+            >
               <ForumIcon className="icon" />
               Chats
             </Typography>
           </Link>
 
           <Link to="/Analytics">
-            <Typography 
-            id="hoverrr"
-            className={highlight ? "hoverrr2" : ""}
-             sx={{ mt: -2 }} onClick={handleHighlight}>
+            <Typography
+              id="hoverrr"
+              className={highlight === "analytics" ? "hoverrr2" : ""}
+              sx={{ mt: -2 }}
+              onClick={() => handleHighlight("analytics")}
+            >
               <SignalCellularAltIcon className="icon" />
               Analytics
             </Typography>
@@ -255,35 +277,41 @@ function SideBar({ openSidebarToggle, OpenSidebar }) {
 
             <Collapse in={openPeople} timeout="auto" unmountOnExit>
               <List component="div">
-               <ul>
-                    <li className="myCourses">
+                <ul>
+                  <li className="myCourses">
                     <Link to="/User" className="textDecoration">
-                    <Typography sx={{ textDecoration: "none" }} className="textDecoration">
-                          User
-                        </Typography>
+                      <Typography
+                        sx={{ textDecoration: "none" }}
+                        className="textDecoration"
+                      >
+                        User
+                      </Typography>
                     </Link>
-                    </li>
-                    <li className="listDesign">
+                  </li>
+                  <li className="listDesign">
                     <Link to="/MyTeam" className="textDecoration">
-                    <Typography className="textDecoration">My Team</Typography>
+                      <Typography className="textDecoration">
+                        My Team
+                      </Typography>
                     </Link>
-                    </li>
-                  </ul>
-                 
+                  </li>
+                </ul>
               </List>
             </Collapse>
           </Link>
 
           <Link to="/Testimonial">
-            <Typography 
-            id="hoverrr"
-            className={highlight ? "hoverrr2" : ""}
-             sx={{ mt: -2 }} onClick={handleHighlight}>
+            <Typography
+              id="hoverrr"
+              className={highlight === "testimonials" ? "hoverrr2" : ""}
+              sx={{ mt: -2 }}
+              onClick={() => handleHighlight("testimonials")}
+            >
               <PersonIcon className="icon" />
               Testimonial
             </Typography>
           </Link>
-          <Link >
+          <Link>
             <ListItemButton onClick={handleHideSubCat} className="listButton">
               <PersonIcon />
               <ListItemText primary="Configuration" className="coursesHead" />
@@ -292,26 +320,41 @@ function SideBar({ openSidebarToggle, OpenSidebar }) {
 
             <Collapse in={hideSubConfig} timeout="auto" unmountOnExit>
               <List component="div">
-               <ul>
-                    <li className="myCourses" onClick={()=>handleCatConfig("category")}>
-                    <Link  className="textDecoration">
-                    <Typography sx={{ textDecoration: "none" }} className="textDecoration">
-                          Category
-                        </Typography>
+                <ul>
+                  <li
+                    className="myCourses"
+                    onClick={() => handleCatConfig("category")}
+                  >
+                    <Link className="textDecoration">
+                      <Typography
+                        sx={{ textDecoration: "none" }}
+                        className="textDecoration"
+                      >
+                        Category
+                      </Typography>
                     </Link>
-                    </li>
-                    <li className="listDesign" onClick={()=>handleCatConfig("duration")}>
-                    <Link  className="textDecoration">
-                    <Typography className="textDecoration">Duration</Typography>
+                  </li>
+                  <li
+                    className="listDesign"
+                    onClick={() => handleCatConfig("duration")}
+                  >
+                    <Link className="textDecoration">
+                      <Typography className="textDecoration">
+                        Duration
+                      </Typography>
                     </Link>
-                    </li>
-                    <li className="listDesign">
-                    <Link  className="textDecoration" onClick={()=>handleCatConfig("teamMember")}>
-                    <Typography className="textDecoration">Team Member</Typography>
+                  </li>
+                  <li className="listDesign">
+                    <Link
+                      className="textDecoration"
+                      onClick={() => handleCatConfig("teamMember")}
+                    >
+                      <Typography className="textDecoration">
+                        Team Member
+                      </Typography>
                     </Link>
-                    </li>
-                  </ul>
-                 
+                  </li>
+                </ul>
               </List>
             </Collapse>
           </Link>
