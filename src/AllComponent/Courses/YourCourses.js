02f24c21fe71dbfcada2822cc96689189cef147d
vidhi chandrakar <vidhichandrakar.1701@gmail.com>
2024-03-17 22:33:01 +0530

    loader

--- src/AllComponent/Courses/YourCourses.js
+++ src/AllComponent/Courses/YourCourses.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import "../CSSFile/YourCourses.css";
 import cardimg from "../../Media/Images/db7187e8-b7cf-47ed-8900-6de89dabde06.png";
 import CourseHeader from "./CoursesHeader";
-import FolderCopyRoundedIcon from "@mui/icons-material/FolderCopyRounded";
+import Backdrop from '@mui/material/Backdrop';
 import AddRoundedIcon from "@mui/icons-material/AddRounded";
 import Button from "@mui/material/Button";
 import Folder from "../../Media/Images/folder.avif";
@@ -12,29 +12,37 @@ import { YourCoursesCardData } from "../../Data/JsonData";
 import SideBar from "../AdminDashboardMain/SideBar";
 import { Link } from "react-router-dom";
 import { getAllCourses } from "../ActionFactory/apiActions";
+import LoaderComponent from "../../Util/LoaderComponent";
 
 
 const YourCourses = () => {
   const [allCourses, setAllCourses] = useState([]);
+  const [loaderState, setLoaderState] = useState(false)
   useEffect(() => {
-    
+    setLoaderState(true);
     getAllCourses({
       callBack: (response) => {
         const userCallBack = response?.data;
         setAllCourses(userCallBack);
+        setLoaderState(false)
       },
     });
   }, []);
-
+  const handleLoader = ()=>{
+    setLoaderState(false)
+  }
   return (
     <div className="grid-container">
+       
       <SideBar />
       <div className="main-container margin20">
         <CourseHeader
           Heading={"Your Courses (3)"}
           subHeading={"Add/View courses of your brand"}
         />
-
+      <LoaderComponent
+      loaderState={loaderState}
+      />
         <SearchBar mt="2%" placeholder="Search by name" />
         <div className="Add-main-cards">
           <div className="card">
