1fd34e932b93d18b0cfb5567a0da3d634d6f2670
Shrishti2698 <shrishti260498@gmail.com>
2024-04-01 01:10:19 +0530

    getting otp by 10 digits no. and keycode 13

--- src/AllComponent/LoginFiles/LoginRight/LoginEmailandPassword.js
+++ src/AllComponent/LoginFiles/LoginRight/LoginEmailandPassword.js
@@ -69,7 +69,6 @@ const LoginEmailandPassword = () => {
       });
     }
     if (loginWay !== "") {
-      // if (getOTP === parseInt(enteredOTP)) {
       const payload = {
         phone_no: phoneNO?.toString(),
         OTP: parseInt(enteredOTP),
@@ -91,83 +90,34 @@ const LoginEmailandPassword = () => {
           console.log(error.response.data.message);
         },
       });
-      // }
     }
   };
-  
+
   const handleLoginByOTP = (value, type, event) => {
     setDisableLogiBtn(false);
-    // console.log(event);
     if (type === "password") {
       setPhoneNo(validatePhoneNo(value, phoneNO));
-      // console.log("validatePhoneNo", validatePhoneNo(value, phoneNO));
     } else if (type === "OTP") {
       setEnteredOTP(value);
     }
   };
 
-
-
-  // const [inputValue, setInputValue] = useState('');
-
-  const handleKeyDown =(event, value, originalNum) => {
-    setDisableLogiBtn(false);
-    
-    if(typeof(phoneNO) !== 'undefined' && phoneNO.length){
-      // if(!validatePhoneNo(phoneNO)){
-   if( phoneNO?.length === 10 && event.keyCode === 13){
-    // if(inputValue?.length === 10 && event.keyCode === 13){
-
-       // if( validatePhoneNo(phoneNO, originalNum) && event.keyCode === 13){
-      console.log("workingg");
-    // handleLoginByOTP(event);
-      setPhoneNo(validatePhoneNo(value, phoneNO, originalNum));
-   }
-  }
-   else{
-    handleLoginByOTP(false);
-   }
-  }
-
-
-  // const handleOtp = (type, value) => {
-  //   setDisableLogiBtn(false);
-  //   let prevOtp = { ...otpValue };
-  //   prevOtp[type] = value;
-  //   if (
-  //     prevOtp.otp1 != "" &&
-  //     prevOtp.otp2 != "" &&
-  //     prevOtp.otp3 != "" &&
-  //     prevOtp.otp4 != ""
-  //     // prevOtp.otp5 != "" &&
-  //     // prevOtp.otp6 != ""
-  //   ) {
-  //     prevOtp.disable = false;
-  //   } else {
-  //     prevOtp.disable = true;
-  //   }
-  //   setOtp(prevOtp);
-  //   console.log("typeee", type);
-  // };
-  // const handleLoginType = () => {
-  //   // cameFrom!=="signUp"?handleLoginOption(cameFrom==="signUp"?"loggIn":"continueOtp"):handleClick()
-  //   let typedOtp = parseInt(
-  //     otpValue.otp1 + otpValue.otp2 + otpValue.otp3 + otpValue.otp4
-  //   );
-
-  //   console.log("loginResponse.OTP", loginResponse?.OTP);
-  //   console.log("otpValueotpValue", typedOtp, parseInt(typedOtp));
-  //   if (loginResponse?.OTP === typedOtp) {
-  //     createUserByID({
-  //       userId: loginResponse.user_id,
-  //       callBack: (response) => {
-  //         localStorage.setItem("userId", loginResponse.user_id);
-  //         // console.log("response use id", response.data.user_name);
-  //         localStorage.setItem("userName", response.data.user_name);
-  //       },
-  //     });
-  //     navigateHome("/AllCourses");
-  //   }1
+  const handleKeyDown = (event, value, originalNum) => {
+    // setDisableLogiBtn(false);
+    console.log(event, phoneNO);
+    if (typeof phoneNO !== "undefined" && phoneNO?.length) {
+      if (phoneNO?.length === 10 && event.keyCode === 13) {
+        console.log("workingg");
+        handleUserLogin();
+      } else if (phoneNO?.length !== 10) {
+        console.log("only 10 digit no.");
+        setLoaderState(false);
+      } else {
+        toast.error("Invalid Phone Number");
+      }
+    }
+  };
+  // 9340290314
 
   return (
     <div className="RightBox">
@@ -208,20 +158,6 @@ const LoginEmailandPassword = () => {
         ) : null}
 
         <Box sx={{ mt: 2 }}>
-          {/* <TextField
-            id="fullWidth"
-            label="Enter Phone No"
-            variant="outlined"
-            className="BoxShadow"
-            fullWidth
-            maxlength="10"
-            disabled={phoneNO?.length===10 && getOTP!==""}
-            type="number"
-            value={phoneNO}
-            onChange={(event) =>
-              handleLoginByOTP(event.target.value, "password")
-            }
-          /> */}
           <TextField
             id="fullWidth"
             placeholder="Mobile Number"
@@ -232,7 +168,7 @@ const LoginEmailandPassword = () => {
             disabled={phoneNO?.length === 10 && getOTP !== ""}
             type="number"
             value={phoneNO}
-            onKeyDown = {(event)=> handleKeyDown(event)}
+            onKeyDown={(event) => handleKeyDown(event)}
             onChange={(event) =>
               handleLoginByOTP(event.target.value, "password", event)
             }
@@ -261,47 +197,6 @@ const LoginEmailandPassword = () => {
               fullWidth
               onChange={(event) => handleLoginByOTP(event.target.value, "OTP")}
             />
-
-            {/* <TextField
-              id="outlined-basic"
-              variant="outlined"
-              className="OTPBox"
-              onChange={(event) => handleOtp("otp1", event.target.value)}
-              inputProps={{
-                maxLength: 1,
-                className: "boxOtpWidth",
-              }}
-            />
-            <TextField
-              id="outlined-basic"
-              variant="outlined"
-              className="OTPBox"
-              onChange={(event) => handleOtp("otp2", event.target.value)}
-              inputProps={{
-                maxLength: 1,
-                className: "boxOtpWidth",
-              }}
-            />
-            <TextField
-              id="outlined-basic"
-              variant="outlined"
-              className="OTPBox"
-              onChange={(event) => handleOtp("otp3", event.target.value)}
-              inputProps={{
-                maxLength: 1,
-                className: "boxOtpWidth",
-              }}
-            />
-            <TextField
-              id="outlined-basic"
-              variant="outlined"
-              className="OTPBox"
-              onChange={(event) => handleOtp("otp4", event.target.value)}
-              inputProps={{
-                maxLength: 1,
-                className: "boxOtpWidth",
-              }}
-            /> */}
           </Box>
         )}
 
