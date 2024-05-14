404a0d78d97dd493f96e7a5e7b38ab48c3469326
Shrishti2698 <shrishti260498@gmail.com>
2024-05-14 21:52:00 +0530

    few logo alignment changes

--- src/AllComponent/DAshBoardRight/People/TestPortal/TestInstructions.jsx
+++ src/AllComponent/DAshBoardRight/People/TestPortal/TestInstructions.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Typography from "@mui/material/Typography";
 import { Box, Button, Divider } from "@mui/material";
 import "./AllTestPortal.css";
@@ -10,21 +10,19 @@ import { styled } from "@mui/material/styles";
 import Dialog from "@mui/material/Dialog";
 import DialogTitle from "@mui/material/DialogTitle";
 import DialogActions from "@mui/material/DialogActions";
-import DialogContent from '@mui/material/DialogContent';
-import IconButton from '@mui/material/IconButton';
-import CloseIcon from '@mui/icons-material/Close';
-import TextField from '@mui/material/TextField';
+import DialogContent from "@mui/material/DialogContent";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+import TextField from "@mui/material/TextField";
 
 function TestInstructions() {
-
-    const [instructions, setInstructions] = React.useState(false);
+  const [instructions, setInstructions] = React.useState(false);
   const handleClickInstruction = () => {
     setInstructions(true);
   };
   const handleClickClose = () => {
     setInstructions(false);
   };
-
 
   const BootstrapDialog = styled(Dialog)(({ theme }) => ({
     "& .MuiDialogContent-root": {
@@ -45,95 +43,97 @@ function TestInstructions() {
 
   return (
     <div>
-     <Box className="stickyTopBox">
-              <div>
-                <h4>
-                  Test Details
-                  <EditIcon className="blueEdit" />
-                </h4>
-              </div>
+      <Box className="stickyTopBox">
+        <div>
+          <h4>
+            Test Details
+            <EditIcon className="blueEdit" />
+          </h4>
+        </div>
 
-              
-              <div style={{ display: "flex", flexDirection: "row" }}>
-                {" "}
-                <AccessTimeIcon className="testDurationLogo" />
-                <Typography style={{ color: "rgb(146 130 130)" }}>
-                  Test Duration :{" "}
-                  <span className="textColorBold">3hrs30mins</span>
-                </Typography>
-                <LocalOfferIcon
-                  className="testDurationLogo"
-                  style={{ marginLeft: "2%" }}
-                />
-                <Typography style={{ color: "rgb(146 130 130)" }}>
-                  Tags :<span className="textColorBold"> TEST MODULE X</span>
-                </Typography>
-              </div>
-
-
-              <Box className="testInstructionsBtn">
-                <p>
-                  <b>Test Instructions:</b>{" "}
-                  <span style={{ color: "#000" }}>
-                    Test Instructions: </span>
-                    <span>
-                        <Button 
-                        // onClick={handleClickInstruction}
-                        onClick={handleClickOpen}>Click here to add </Button>
-                   
-
-      <BootstrapDialog
-        onClose={handleClose}
-        aria-labelledby="customized-dialog-title"
-        open={open}
-      >
-        <DialogTitle sx={{ m: 0, p: 2, alignItems: "center", textAlign: "center" }} id="customized-dialog-title">
-          Test Instructions
-        </DialogTitle>
-        <IconButton
-          aria-label="close"
-          onClick={handleClose}
-          sx={{
-            position: 'absolute',
-            right: 8,
-            top: 8,
-            color: (theme) => theme.palette.grey[500],
-          }}
-        >
-          <CloseIcon />
-        </IconButton>
-
-
-        <DialogContent dividers>
-          <Typography gutterBottom>
-            <b>Test Instructions</b>
+        <div style={{ display: "flex", flexDirection: "row" }}>
+          {" "}
+          <AccessTimeIcon className="testDurationLogo" />
+          <Typography style={{ color: "rgb(146 130 130)" }}>
+            Test Duration : <span className="textColorBold">3hrs30mins</span>
           </Typography>
-            <Box className="testInstTextField"
-      component="form"
-      sx={{
-        '& > :not(style)': { m: 1, width: '25ch' },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <TextField id="outlined-basic" variant="outlined" />
-    </Box>
-        </DialogContent>
+          <LocalOfferIcon
+            className="testDurationLogo"
+            style={{ marginLeft: "2%" }}
+          />
+          <Typography style={{ color: "rgb(146 130 130)" }}>
+            Tags :<span className="textColorBold"> TEST MODULE X</span>
+          </Typography>
+        </div>
 
+        <Box className="testInstructionsBtn">
+          <p>
+            <b>Test Instructions:</b>{" "}
+            <span style={{ color: "#000" }}>Test Instructions: </span>
+            <span>
+              <Button
+                onClick={handleClickOpen}
+              >
+                Click here to add{" "}
+              </Button>
 
-        <DialogActions>
-          <Button autoFocus onClick={handleClose} className="doneBtnInstPage">
-            Done
-          </Button>
-        </DialogActions>
-      </BootstrapDialog>
-      </span>
+              <BootstrapDialog
+                onClose={handleClose}
+                aria-labelledby="customized-dialog-title"
+                open={open}
+              >
+                <DialogTitle
+                  sx={{ m: 0, p: 2, alignItems: "center", textAlign: "center" }}
+                  id="customized-dialog-title"
+                >
+                  Test Instructions
+                </DialogTitle>
+                <IconButton
+                  aria-label="close"
+                  onClick={handleClose}
+                  sx={{
+                    position: "absolute",
+                    right: 8,
+                    top: 8,
+                    color: (theme) => theme.palette.grey[500],
+                  }}
+                >
+                  <CloseIcon />
+                </IconButton>
 
-                </p>
-              </Box>
-            </Box>    
+                <DialogContent dividers>
+                  <Typography gutterBottom>
+                    <b>Test Instructions</b>
+                  </Typography>
+                  <Box
+                    className="testInstTextField"
+                    component="form"
+                    sx={{
+                      "& > :not(style)": { m: 1, width: "25ch" },
+                    }}
+                    noValidate
+                    autoComplete="off"
+                  >
+                    <TextField id="outlined-basic" variant="outlined" />
+                  </Box>
+                </DialogContent>
+
+                <DialogActions>
+                  <Button
+                    autoFocus
+                    onClick={handleClose}
+                    className="doneBtnInstPage"
+                  >
+                    Done
+                  </Button>
+                </DialogActions>
+              </BootstrapDialog>
+            </span>
+          </p>
+        </Box>
+      </Box>
     </div>
-  )
+  );
 }
 
-export default TestInstructions
+export default TestInstructions;
