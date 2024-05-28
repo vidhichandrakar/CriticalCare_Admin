d9dbded3756c5e183ad90d059f3b29a8aec2b5fa
Shrishti2698 <shrishti260498@gmail.com>
2024-05-29 01:10:47 +0530

    add subjective test created

--- src/AllComponent/Courses/AddContent/AddContent/SubjectiveTest.jsx
+++ src/AllComponent/Courses/AddContent/AddContent/SubjectiveTest.jsx
@@ -13,7 +13,13 @@ import InboxIcon from "@mui/icons-material/MoveToInbox";
 import MailIcon from "@mui/icons-material/Mail";
 import IconButton from "@mui/material/IconButton";
 import CloseIcon from "@mui/icons-material/Close";
-import "../../../CSSFile/Courses.css"
+import "../../../CSSFile/Courses.css";
+import Paper from "@mui/material/Paper";
+import InputBase from "@mui/material/InputBase";
+// import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
+
+
 
 const SubjectiveTest = (anchor) => {
   const [state, setState] = React.useState({
@@ -52,7 +58,7 @@ const SubjectiveTest = (anchor) => {
       onKeyDown={toggleDrawer(anchor, false)}
     >
       {/* <Box className="HeaderBox"> */}
-      <h2 style={{marginLeft: "4%"}}>Add SubjectiveTest</h2>
+      <h2 style={{marginLeft: "4%"}}>Add Subjective Test</h2>
       <IconButton
         aria-label="close"
         onClick={handleClose}
@@ -68,8 +74,8 @@ const SubjectiveTest = (anchor) => {
       {/* </Box> */}
       <Divider />
 
-      <Box className="FolderNameBox">
-        <h4>Folder name</h4>
+      <Box className="addSubjUploadBox">
+        {/* <h4>Folder name</h4>
         <Box
           sx={{
             width: 500,
@@ -77,11 +83,48 @@ const SubjectiveTest = (anchor) => {
           }}
         >
           <TextField fullWidth label="fullWidth" id="fullWidth" />
-        </Box>
+        </Box> */}
+        <div>
+          <p>Upload your subjective test papers & create a new test within a minute!</p>
+        </div>
+        
+        <Button className="createNewTestBtn">Create New Test</Button>
       </Box>
 
-      <Divider sx={{ mt: "348px" }} />
-      <Button className="folderDrawerSaveBtn">Save</Button>
+      <p className="subjBoldPara"><b>You can also select from previously created tests</b></p>
+
+      <Paper
+      component="form"
+      sx={{
+        ml: "36px",
+        mt: "56px",
+        mb: "-9px",
+        display: "flex",
+        alignItems: "center",
+        width: "594px",
+        backgroundColor: "#e4e4e459",
+        borderRadius: "10px"
+      }}
+    >
+      <IconButton sx={{ p: '10px' }} aria-label="menu">
+        <SearchIcon />
+      </IconButton>
+      {/* <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" /> */}
+      <InputBase
+        sx={{ ml: 1, flex: 1 }}
+        placeholder="Search by name"
+        inputProps={{ 'aria-label': 'search your course by name' }}
+      />
+    </Paper>
+
+    <Box className="A-searchImageSection">
+      <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.iconfinder.com%2Ficons%2F749535%2Fa_document_find_letter_search_icon&psig=AOvVaw3fZLu9FmZUBiiqwVB5lq14&ust=1717010672877000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLDUsNaJsYYDFQAAAAAdAAAAABAE"
+       width={120} height={110} sx={{borderRadius:"50%"}} />
+      <h3>No Result found</h3>
+    </Box>
+      
+      <Divider sx={{ mt: "53px" }} />
+      <Button className="folderDrawerSaveBtn">Add test</Button>
     </Box>
   );
 };
