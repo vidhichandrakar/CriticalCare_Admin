59d9bdbb42c58da22551b508275ab29f59cc753a
vidhi chandrakar <vidhichandrakar.1701@gmail.com>
2024-12-24 13:53:57 +0530

    changes in tripmHtmlTagsToNormalFormat

--- src/Util/CommonHtmlTagsToTextConvertor.js
+++ src/Util/CommonHtmlTagsToTextConvertor.js
@@ -3,3 +3,8 @@ export const tripmHtmlTagsToNormalFormat = (htmlString) => {
   const doc = parser.parseFromString(htmlString, "text/html");
   return doc.body.textContent || "";
 };
+export const tripmHtmlTagsToNormalFormatinside = (htmlString) => {
+ 
+  return htmlString ? <div dangerouslySetInnerHTML={{ __html: htmlString }} style={{marginLeft:"3%", marginTop:"2%"}}/> : ""
+
+};
